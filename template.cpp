366991dc74965e83d3f0e02289ebc5fed7e90863
yay <61081228+yanganyi@users.noreply.github.com>
2024-07-29 14:25:40 +0800

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+int32_t main(){
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+    cout<<"hello world";
+    
+    return 0;
+}
